6086935cbc57db3594d8938b4ca703751ad8c5ad
GgprDuck <sashacazachcov91@gmail.com>
2022-09-20 00:27:52 +0300

    final

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -3,12 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { BooksController } from './books.controller';
 import { BooksService } from './books.service';
 import { Book, BookSchema } from './schemas/books.schema';
+import { UsersRepository } from 'src/users/users.repository';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name:Book.name, schema: BookSchema }]),
   ],
   controllers: [BooksController],
-  providers: [BooksService],
+  providers: [BooksService, UsersRepository],
 })
 export class BooksModule {}
