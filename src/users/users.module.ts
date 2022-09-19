61f32352148fb5411d560e77fadfbbda03f058c0
GgprDuck <sashacazachcov91@gmail.com>
2022-09-20 00:05:14 +0300

    final

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,12 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { User, UserSchema } from './schemas/users.schema';
+import { UsersRepository } from './users.repository';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name:User.name, schema: UserSchema }]),
   ],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, UsersRepository],
 })
 export class UsersModule {}
